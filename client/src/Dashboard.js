4977686411344314632987e604e95aa95d06e96d
Henning Lin <202007040@post.au.dk>
2023-05-11 11:31:57 +0200

    trying to fix playlist

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -2,16 +2,19 @@ import { useState, useEffect } from "react";
 import useAuth from "./useAuth";
 import Player from "./Player";
 import TrackSearchResult from "./TrackSearchResult";
-import { Container, Form, Navbar, Nav } from "react-bootstrap";
+import { Container, Form, Navbar, Nav, Carousel } from "react-bootstrap";
 import SpotifyWebApi from "spotify-web-api-node";
 import axios from "axios";
 import LikeDislike from "./LikeDislike";
 import Data from "./Data";
 import Participants from "./Participants";
 import History from "./History";
+import {auth, db} from "./firebase";
+import Level from "./Level";
+import { addDoc, collection, serverTimestamp, updateDoc, limit, orderBy, query, onSnapshot, getDocs, where, doc, getDoc, deleteDoc } from "firebase/firestore"; 
 
 const spotifyApi = new SpotifyWebApi({
-  clientId: " df5386eb382b4286a239d80f6b301967",
+  clientId: "df5386eb382b4286a239d80f6b301967",
 });
 
 export default function Dashboard({ code }) {
@@ -21,24 +24,224 @@ export default function Dashboard({ code }) {
   const [playingTrack, setPlayingTrack] = useState();
   const [lyrics, setLyrics] = useState("");
   const [activeTab, setActiveTab] = useState("Home");
-
+  const [queuedSongs, setQueuedSongs] = useState([]);
+  const [selectedSongId, setSelectedSongId] = useState(null);
 
   function chooseTrack(track) {
+
     console.log("Selected track: ", track);
     setPlayingTrack(track);
     setSearch("");
     setLyrics("");
+
+    const partyKeyword = localStorage.getItem("partyKeyword");
+    const userId = auth.currentUser;
+    
+    //Add track to firebase
+    async function addTrackToFirestore() {
+      try {
+        const trackDetails = await spotifyApi.getTrack(track.uri.split(":")[2]);
+        const artistId = trackDetails.body.artists[0].id;
+
+        const genres = await getArtistGenres(artistId);
+
+        const partySongsRef = collection(db, "Parties", partyKeyword, "searchedSongs");
+
+        const q = query(collection(db, "Parties", partyKeyword, "searchedSongs"), where("uri", "==", track.uri));
+
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+          console.log("Track already in queue");
+          alert("Song already in queue");
+          return;
+        }
+
+        const songDuration = trackDetails.body.duration_ms / 1000;
+  
+        const docRef = await addDoc(partySongsRef, {
+          name: track.title,
+          artist: track.artist,
+          uri: track.uri,
+          albumUrl: track.albumUrl,
+          user: userId.displayName,
+          timestamp: serverTimestamp(),
+          genre: genres,
+          score: 0,
+          priority: 0,
+          duration: songDuration
+        });
+        
+        console.log("Document written with ID: ", docRef.id);
+        console.log("Added song to database: ", track.title);
+
+        return docRef.id;
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    }
+    
+    addTrackToFirestore().then(async(songId)=>{
+      setSelectedSongId(songId);
+      console.log(songId);
+
+      // Fetch the playlistId from Firebase and call addTrackToPlaylist()
+      const partyKeyword = localStorage.getItem("partyKeyword");
+      const playlistId = await fetchPlaylistId(partyKeyword);
+
+      if(playlistId){
+        const partySongsRef = collection(db, "Parties", partyKeyword, "searchedSongs");
+        const songQuery = query(partySongsRef, where("addedToPlaylist", "==", false), orderBy("timestamp", "asc"), limit(1));
+        const songSnap = await getDocs(songQuery);
+
+        if (!songSnap.empty) {
+          const oldestSong = songSnap.docs[0];
+          // Add the song with the oldest timestamp to the Spotify playlist
+          addTrackToPlaylist(playlistId, oldestSong.data().uri);
+    
+          // Update the song in Firestore to mark it as added to the playlist
+          await updateDoc(doc(partySongsRef, oldestSong.id), {
+            addedToPlaylist: true
+          });
+        } else {
+          console.error("Could not fetch song with the oldest timestamp");
+        }
+
+      } else {
+        console.error("Could not fetch playlistId");
+      }
+    });
   }
+
+  //Get genres from track
+  async function getArtistGenres(artistId) {
+    try {
+      const artistDetails = await spotifyApi.getArtist(artistId);
+      return artistDetails.body.genres;
+    } catch (error) {
+      console.error("Error fetching artist genres:", error);
+      return [];
+    }
+  }
+
+  //Create playlist in Spotify
+  async function createPlaylist(accessToken) {
+    spotifyApi.setAccessToken(accessToken);
+    const partyKeyword = localStorage.getItem("partyKeyword");
+    console.log("Party keyword:", partyKeyword);
+    const partyDocRef = doc(db, "Parties", partyKeyword);
+    const docSnapshot = await getDoc(partyDocRef);
+    console.log("Document snapshot:", docSnapshot);
+
+    if (!docSnapshot.exists || !docSnapshot.data().playlist) {
+
+      //Get party details from firebase
+      const partyData = docSnapshot.data()
+      const partyName = partyData.name;
+      const partyDate = partyData.date;
+
+      // Create the playlist
+      const playlistName = `${partyName} - ${partyDate}`;
+      const playlistData = await spotifyApi.createPlaylist(playlistName, { 'public': true, 'collaborative': true })
+      const playlistId = playlistData.body.id;
+
+      // Update the Firebase document with the new playlist field
+      await updateDoc(partyDocRef, { playlist: playlistId })
+      .then(() => {
+        console.log("Document updated successfully");
+      })
+      .catch((error) =>{
+        console.error("Error updating document: ", error);
+      });
+
+      console.log("Playlist created in firebase and spotify", playlistId);
+      return playlistId;
+
+    } else {
+      console.log("Playlist for the party already exists");
+      console.log("Playlist data:", docSnapshot.data());
+      return docSnapshot.data().playlist;
+    }
+  }
+
+  //Add song to Spotify
+  async function addTrackToPlaylist(playlistId, trackUri) {
+    try {
+      const response = await spotifyApi.addTracksToPlaylist(playlistId, [trackUri]);
+      console.log("Song added to spotify: ", response);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  //Fetch playlist variable from firebase
+  async function fetchPlaylistId(partyKeyword) {
+    try {
+      const partyDocRef = doc(db, "Parties", partyKeyword);
+      const docSnapshot = await getDoc(partyDocRef);
+  
+      if (docSnapshot.exists()) {
+        const playlistId = docSnapshot.data().playlist;
+        return playlistId;
+      } else {
+        console.error("Party document does not exist");
+      }
+    } catch (err) {
+      console.error("Error fetching playlistId: ", err);
+    }
+  }
+
+  async function deleteSong(songId) {
+    const partyKeyword = localStorage.getItem("partyKeyword");
+    const songRef = doc(db, "Parties", partyKeyword, "searchedSongs", songId);
+  
+    try {
+      await deleteDoc(songRef);
+      console.log(`Deleted song with id: ${songId}`);
+    } catch (error) {
+      console.error(`Error deleting song: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    if(accessToken){
+      console.log("Creating playlist");
+      createPlaylist();
+    }
+  }, [accessToken]);
 
   useEffect(() => {
     console.log("Access token:", accessToken);
   }, [accessToken]);
+
+  //Show queued songs as a carousel.
+  useEffect(() => {
+    const partyKeyword = localStorage.getItem("partyKeyword");
+  
+    if (partyKeyword) {
+      const partySongsRef = collection(db, "Parties", partyKeyword, "searchedSongs");
+  
+      const unsub = onSnapshot(
+        query(partySongsRef, orderBy("timestamp", "asc")),
+        (snapshot) => {
+          const queuedSongsData = [];
+          snapshot.forEach((doc) => {
+            queuedSongsData.push({ id: doc.id, ...doc.data() });
+          });
+          setQueuedSongs(queuedSongsData);
+          console.log("All fetched songs: ", queuedSongsData);
+        },
+        (error) => console.error("Error fetching queued songs: ", error)
+      );
+       
+      return () => unsub();
+    }
+  }, []);
   
   //Used to show lyrics from the song playing
   useEffect(() => {
     if (!playingTrack) return setLyrics("Search a song to display lyrics");
     axios
-      .get("http://localhost:3001/lyrics", {
+      .get("https://musicify-lin.herokuapp.com/lyrics", {
         params: {
           track: playingTrack.title,
           artist: playingTrack.artist,
@@ -53,7 +256,8 @@ export default function Dashboard({ code }) {
     if (!accessToken) return console.log("No access token");
     spotifyApi.setAccessToken(accessToken);
   }, [accessToken]);
-
+  
+  //Search result, show title, image, artist.
   useEffect(() => {
     if (!search) return setSearchResults([]);
     if (!accessToken) return "No access";
@@ -84,6 +288,7 @@ export default function Dashboard({ code }) {
     return () => (cancel = true);
   }, [search, accessToken]);
 
+  //Logout of the app
   function handleLogout() {
     localStorage.removeItem("spotify-auth")
     window.location = "/";
@@ -100,7 +305,7 @@ export default function Dashboard({ code }) {
         <Nav>
           <Nav.Link active={activeTab === "Home"} onClick={() => setActiveTab("Home")}>Home</Nav.Link>
           <Nav.Link active={activeTab === "Lyrics"} onClick={() => setActiveTab("Lyrics")}>Lyrics</Nav.Link>
-          <Nav.Link active={activeTab === "Data"} onClick={() => setActiveTab("Data")}>Genre</Nav.Link>
+          <Nav.Link active={activeTab === "Data"} onClick={() => setActiveTab("Data")}>Data</Nav.Link>
           <Nav.Link active={activeTab === "History"} onClick={() => setActiveTab("History")}>Playlist</Nav.Link>
           <Nav.Link active={activeTab === "Participants"} onClick={() => setActiveTab("Participants")}>Users</Nav.Link>
           <Nav.Link onClick={handleLogout}>Exit</Nav.Link>
@@ -126,15 +331,29 @@ export default function Dashboard({ code }) {
         {searchResults.length === 0 && (
           <>{activeTab === "Home" && (
             <div className="d-flex justify-content-center align-items-center mb-2">
-              {playingTrack && (
-                <div className="justify-content-center">
-                  <h4 style= {{ textAlign: 'center' }}>{playingTrack.title}</h4>
-                  <p className="text-muted" style= {{ textAlign: 'center' }}>{playingTrack.artist}</p>
-                <div style={{ width: '180px', height: '180px', margin: '0 auto' }}>
-                  <img src={playingTrack.albumUrl} alt={playingTrack.title} style={{width: "100%", height: "100%", objectFit: 'cover'}}/>
-                </div>
-                <LikeDislike />
-                </div>
+              {queuedSongs.length ? (
+                <Carousel indicators={false} variant="dark">
+                 {queuedSongs.map((song, idx) => (
+                  <Carousel.Item key={idx}>
+                    <div className="d-flex justify-content-center align-items-center mb-2">
+                      <div className="justify-content-center">
+                        <h4 style={{ textAlign: 'center' }}>{song.name}</h4>
+                        <p className="text-muted" style={{ textAlign: 'center' }}>{song.artist}</p>
+                        <div style={{ width: '200px', height: '200px', margin: '0 auto' }}>
+                          <img
+                            src={song.albumUrl}
+                            alt={song.name}
+                            style={{ width: "100%", height: "100%", objectFit: 'cover' }}
+                          />
+                        </div>
+                        <LikeDislike songId={song.id}/>
+                      </div>
+                    </div>
+                  </Carousel.Item>
+                ))}
+                </Carousel>
+              ) : (
+                <p>No songs queued yet</p>
               )}
             </div>
             )}
@@ -163,11 +382,15 @@ export default function Dashboard({ code }) {
           </>
         )}
       </div>
+      <div className="justify-content-center align-items-center mb-2">
+        <Level/>
+      </div>
       <div>
-        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+        {/*<Player accessToken={accessToken}/>*/}
       </div>
       
     </Container>
     </div>
   );
 }
+
